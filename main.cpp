e27018d2fd60df2c53354828204696d759787f52
Tomasz Tendera <tomektendera@gmail.com>
2023-05-18 21:24:30 +0200

    Clear and rename conversion methods

--- main.cpp
+++ main.cpp
@@ -18,52 +18,26 @@ typedef struct HSL_Color
 	int L;
 }HSL_Color;
 
-void RGBConvertToHSL(RGB_Color *src, HSL_Color *dest);
+void RGBToHSL(int R, int G, int B);
 
-void HSLConvertToRGB(HSL_Color *src, RGB_Color *dest);
+void HSLToRGB(int H, int S, int L);
 
 int main()
 {
-	RGB_Color a = { .R = 251,
-		   	.G = 30,
-			.B = 21};
-	HSL_Color a1 = {0};
-
-	HSL_Color b = { .H = 0,
-			.S = 100,
-			.L = 50};
-	RGB_Color b1 = {0};
-	RGBConvertToHSL(&a, &a1);
-//	HSLConvertToRGB(&b, &b1);
+    int R, G, B;
+    int H, S, L;
+    RGBToHSL(R, G, B);
+    HSLToRGB(H, S, L);
 	return 0;
 }
 
-void RGBConvertToHSL(RGB_Color *src, HSL_Color *dest)
+void RGBToHSL(int R, int G, int B)
 {
 	//Using algorithm from:
 	//had2know.org/technology/hsv-rgb-conversion-formula-calculator.html
-	float H, M, m, V, S, temp;
-
-	cout<<"R "<<src->R<<endl;
-	cout<<"G "<<src->G<<endl;
-	cout<<"B "<<src->B<<endl;
-
-	M = (float)max({src->R, src->G, src->B});
-	m = (float)min({src->R, src->G, src->B});
-
-	V = M/(float)255;
-	M>0 ? S = 1-m/M : S=0;
-	H = acos((src->R - 0.5*src->G - 0.5*src->B) / sqrt((src->R * src->R) + (src->G * src->G) + (src->B * src->B) - (src->R * src->G) - (src->R * src->B) - (src->G * src->B)));
-	if(src->B > src->G) H = 360 - H;
-	cout<<"H "<<H<<endl;
-	cout<<"S "<<S<<endl;
-	cout<<"L "<<V<<endl;
 }
 
-void HSLConvertToRGB(HSL_Color *src, RGB_Color *dest)
+void HSLToRGB(int H, int S, int L)
 {
-	cout<<"H "<<src->H<<endl;
-	cout<<"S "<<src->S<<endl;
-	cout<<"L "<<src->L<<endl;
 	cout<<"Convert HSL to RGB"<<endl;
 }
