09eb447d4baabc4946b6c7e4526c1764a81fe6f9
Tendera, Tomasz <tomasz.tendera@merit-automotive.com>
2023-05-23 13:07:03 +0200

    HSL2RGB function

--- main.cpp
+++ main.cpp
@@ -22,11 +22,48 @@ void RGBToHSL(int R, int G, int B);
 
 void HSLToRGB(int H, int S, int L);
 
+float Hue2RGB(float v1, float v2, float vH )
+{
+    if ( vH < 0 ) vH += 1;
+    if( vH > 1 ) vH -= 1;
+    if ( ( 6 * vH ) < 1 ) return ( v1 + ( v2 - v1 ) * 6 * vH );
+    if ( ( 2 * vH ) < 1 ) return ( v2 );
+    if ( ( 3 * vH ) < 2 ) return ( v1 + ( v2 - v1 ) * ( ( 2 / 3 ) - vH ) * 6 );
+    return ( v1 );
+}
+
+
+void HSL2RGB(float H, float S, float L)
+{
+    //Using algorithm from http://www.easyrgb.com/en/math.php#text19
+    float R, G, B, var_1, var_2;
+    if ( S == 0 )
+    {
+        R = L * 255.0;
+        G = L * 255.0;
+        B = L * 255.0;
+    }
+    else
+    {
+        if ( L < 0.5 ) var_2 = L * ( 1.0 + S );
+        else           var_2 = ( L + S ) - ( S * L );
+
+       var_1 = 2.0 * L - var_2;
+
+        R = 255.0 * Hue2RGB( var_1, var_2, H + ( 1.0 / 3.0 ) );
+        G = 255.0 * Hue2RGB( var_1, var_2, H );
+        B = 255.0 * Hue2RGB( var_1, var_2, H - ( 1.0 / 3.0 ) );
+    }
+    cout<<R<<endl;
+    cout<<G<<endl;
+    cout<<B<<endl;
+}
 int main()
 {
     int R = 255, G = 255, B = 255;
-    //int H, S, L;
-    RGBToHSL(R, G, B);
+    //RGBToHSL(R, G, B);
+    float H = 360.0, S = 0.8, L = 0.12;
+    HSL2RGB(H, S, L);
     //HSLToRGB(H, S, L);
 	return 0;
 }
