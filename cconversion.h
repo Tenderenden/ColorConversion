597e0a1ef6a9e8ceb101e291ac4d673f6e798bc8
Tendera, Tomasz <tomasz.tendera@merit-automotive.com>
2023-06-19 16:01:47 +0200

    Create structs for color types

--- cconversion.h
+++ cconversion.h
@@ -2,6 +2,20 @@
 #include <algorithm>
 namespace ColorConv
 {
+    typedef struct 
+    {
+        char R;
+        char G; 
+        char B;
+    }RGB_Color_T;
+
+    typedef struct
+    {
+        float H;
+        float S;
+        float V;
+    }HSL_Color_T;
+    
     void RGB2HSL(int R, int G, int B);
     void HSL2RGB(float H, float S, float L);
 }
